07bb58556a88f51dfed242d4f811151efc33d70a
Thiago <thidurante@hotmail.com>
2022-07-20 18:44:28 -0300

    Ex 2 e 3 Aula 4.1

--- Fundamentos/Javascript/Exercícios.js
+++ Fundamentos/Javascript/Exercícios.js
@@ -1,10 +1,25 @@
 const a = 5;
-const b = 3;
+const b = 8;
+const c = 10;
 
-const sum = a + b;
-const subtract = a - b;
-const multiply = a * b;
-const divide = a / b;
-const mod = a % b;
+// const sum = a + b;
+// const subtract = a - b;
+// const multiply = a * b;
+// const divide = a / b;
+// const mod = a % b;
 
-console.log(sum, subtract, multiply, divide, mod);
+// console.log(sum, subtract, multiply, divide, mod);
+
+if (a > b) {
+  console.log(a);
+} else {
+  console.log(b);
+}
+
+if (a > b && a > c) {
+  console.log(a);
+} else if (b > c) {
+  console.log(b);
+} else {
+  console.log(c);
+}
