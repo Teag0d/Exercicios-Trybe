43b485a1e9f08b80a098572c8803146b480e2b10
Thiago <thidurante@hotmail.com>
2022-07-20 18:40:54 -0300

    Ex 1 Aula 4.1

---
+++ Fundamentos/Javascript/Exercícios.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 3;
+
+const sum = a + b;
+const subtract = a - b;
+const multiply = a * b;
+const divide = a / b;
+const mod = a % b;
+
+console.log(sum, subtract, multiply, divide, mod);
