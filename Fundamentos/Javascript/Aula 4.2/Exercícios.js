a120426319f7fa5cca35e956d37df8c96c5affae
Thiago <thidurante@hotmail.com>
2022-07-21 18:16:27 -0300

    Exercícios 1 a 9

---
+++ Fundamentos/Javascript/Aula 4.2/Exercícios.js
@@ -0,0 +1,54 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// ex1
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+// ex 2
+// let sum = 0
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i]
+//   console.log(sum);
+// }
+// ex 3 e 4
+// let sum = 0
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i]
+// }
+// console.log(sum/numbers.length-1);
+
+// // ex 5
+// let biggestNumber = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > biggestNumber) {
+//     biggestNumber = numbers[i];
+//   }
+// }
+// console.log(biggestNumber);
+
+//ex 6
+// let oddcount = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 !== 0) {
+//     oddcount++;
+//   }
+// }
+// console.log(oddcount);
+
+// ex 7
+// let smallestNumber = numbers[0];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] < smallestNumber) {
+//     smallestNumber = numbers[i];
+//   }
+// }
+// console.log(smallestNumber);
+
+// ex 8 e 9
+// let myArray = [];
+// for (let i = 1; i <= 25; i++) {
+//   myArray.push(i);
+// }
+// console.log(myArray);
+// for (let i = 0; i< myArray.length; i++) {
+//   console.log(myArray[i]/2);
+// }
