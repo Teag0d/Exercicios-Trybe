4ea1449e9424a34afd982804a965c0d8d4e0b836
Thiago <thidurante@hotmail.com>
2022-07-25 18:17:50 -0300

    Exercícios 4.4

---
+++ Fundamentos/Javascript/Aula 4.4/Exercícios.js
@@ -0,0 +1,57 @@
+// let info = {
+//   personagem: "Margarida",
+//   origem: "Pato Donald",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+//   recorrente: "Sim",
+// };
+// let info2 = {
+//   personagem: "Tio Patinhas",
+//   origem: "Christmas on Bear Mountain, Dells Four Color Comics #178",
+//   nota: "O último MacPatinhas",
+//   recorrente: "Sim",
+// };
+
+// console.log('Bem-vinda, ', info.personagem);
+// console.log(info)
+
+// for (let property in info) {
+//   console.log(property);
+// }
+
+// for (let key in info) {
+//   console.log(info[key]);
+// }
+// console.table(info);
+// console.log(info.personagem + " e " + info2.personagem);
+// console.log(info.origem + " e " + info2.origem);
+// console.log(info.nota + " e " + info2.nota);
+// if (info.recorrente === info2.recorrente){
+//   console.log('Ambos Recorrentes')
+// }
+
+// ex 6, 7 e 8
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+};
+
+console.log(
+  `O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ` +
+    leitor.livrosFavoritos[0].titulo
+);
+leitor.livrosFavoritos[1] = {
+  titulo: "Harry Potter e o Prisioneiro de Azkaban",
+  autor: "JK Rowling",
+  editora: "Rocco",
+};
+
+console.log(leitor.livrosFavoritos);
+console.log("Julia tem ", leitor.livrosFavoritos.length, " livros favoritos");
