331aaa3a4a00cff74b057e950fd37dae172a7917
Thiago <thidurante@hotmail.com>
2022-08-25 19:27:38 -0300

    1 2 3 8.5

--- Fundamentos/B8 HoFs/Aula 8.4/Exercícios.js
+++ Fundamentos/B8 HoFs/Aula 8.4/Exercícios.js
@@ -79,75 +79,75 @@ const reduceNames = (arr) =>
 
 console.log(reduceNames(books));
 
-const averageAge = (arr) => {
-  const sumOfAges = arr.reduce(
-    (acc, curr) => {
-      Object.keys(acc).includes('count') ? acc.count++ : (acc.count = 1);
-      acc.age += curr.releaseYear - curr.author.birthYear;
-      return acc;
-    },
-    { age: 0 }
-  );
-  console.log(sumOfAges);
-  return sumOfAges.age / sumOfAges.count;
-};
+// const averageAge = (arr) => {
+//   const sumOfAges = arr.reduce(
+//     (acc, curr) => {
+//       Object.keys(acc).includes('count') ? acc.count++ : (acc.count = 1);
+//       acc.age += curr.releaseYear - curr.author.birthYear;
+//       return acc;
+//     },
+//     { age: 0 }
+//   );
+//   console.log(sumOfAges);
+//   return sumOfAges.age / sumOfAges.count;
+// };
 
-console.log(averageAge(books));
+// console.log(averageAge(books));
 
-const longestNamedBook = (arr) =>
-  arr.reduce((acc, curr) => {
-    acc.name.length < curr.name.length ? (acc = curr) : acc;
-    return acc;
-  });
-// longestNamedBook(books);
-console.log(longestNamedBook(books));
+// const longestNamedBook = (arr) =>
+//   arr.reduce((acc, curr) => {
+//     acc.name.length < curr.name.length ? (acc = curr) : acc;
+//     return acc;
+//   });
+// // longestNamedBook(books);
+// console.log(longestNamedBook(books));
 
-const names = [
-  'Aanemarie',
-  'Adervandes',
-  'Akifusa',
-  'Abegildo',
-  'Adicellia',
-  'Aladonata',
-  'Abeladerco',
-  'Adieidy',
-  'Alarucha',
-];
-
-const containsA = (arr) => {
-  return arr.reduce(
-    //acc é só um elemento sem ligação direta com o que estamos iterando, curr é algo da lista que estamos passando por
-    (acc, currWord) =>
-      acc +
-      currWord.split('').reduce((accu, currLetter) => {
-        // mesma coisa aqui, reduce só funciona em arrays, split transforma pra array
-        currLetter === 'A' || currLetter === 'a' ? (accu += 1) : accu;
-        return accu;
-      }, 0),
-    0
-  );
-};
-
-console.log(containsA(names));
-
-const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
-const grades = [
-  [9, 8, 10, 7, 5],
-  [10, 9, 9, 10, 8],
-  [10, 7, 10, 8, 9],
-];
-
-const studentAverage = (studentsArr, gradesArr) => {
-  return (nameAndAverage = studentsArr.map((student, index) => ({
-    name: student,
-    average:
-      grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length,
-  })));
-};
-
-console.log(studentAverage(students, grades));
-// const expected = [
-//   { name: 'Pedro Henrique', average: 7.8 },
-//   { name: 'Miguel', average: 9.2 },
-//   { name: 'Maria Clara', average: 8.8 },
+// const names = [
+//   'Aanemarie',
+//   'Adervandes',
+//   'Akifusa',
+//   'Abegildo',
+//   'Adicellia',
+//   'Aladonata',
+//   'Abeladerco',
+//   'Adieidy',
+//   'Alarucha',
 // ];
+
+// const containsA = (arr) => {
+//   return arr.reduce(
+//     //acc é só um elemento sem ligação direta com o que estamos iterando, curr é algo da lista que estamos passando por
+//     (acc, currWord) =>
+//       acc +
+//       currWord.split('').reduce((accu, currLetter) => {
+//         // mesma coisa aqui, reduce só funciona em arrays, split transforma pra array
+//         currLetter === 'A' || currLetter === 'a' ? (accu += 1) : accu;
+//         return accu;
+//       }, 0),
+//     0
+//   );
+// };
+
+// console.log(containsA(names));
+
+// const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+// const grades = [
+//   [9, 8, 10, 7, 5],
+//   [10, 9, 9, 10, 8],
+//   [10, 7, 10, 8, 9],
+// ];
+
+// const studentAverage = (studentsArr, gradesArr) => {
+//   return (nameAndAverage = studentsArr.map((student, index) => ({
+//     name: student,
+//     average:
+//       grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length,
+//   })));
+// };
+
+// console.log(studentAverage(students, grades));
+// // const expected = [
+// //   { name: 'Pedro Henrique', average: 7.8 },
+// //   { name: 'Miguel', average: 9.2 },
+// //   { name: 'Maria Clara', average: 8.8 },
+// // ];
