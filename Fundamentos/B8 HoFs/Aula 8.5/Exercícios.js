2a6ce273036f9532d39de3a732886b3b248009f4
Thiago <thidurante@hotmail.com>
2022-09-05 03:46:34 -0300

    Testing

---
+++ Fundamentos/B8 HoFs/Aula 8.5/Exercícios.js
@@ -0,0 +1,37 @@
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => {
+  console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
+});
+
+const sum = (...numbers) => numbers.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum(4, 5, 6));
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+const personLikes = ({ name, age, likes }) =>
+  `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+// <!-- <nome> tem <anos de idade> e gosta de <gostos da pessoa> -->
+
+// <!-- Retornos esperados: -->
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
