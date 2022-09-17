06e6dbde42dd362d8c7467289eb95bb1fd59f583
Thiago <thidurante@hotmail.com>
2022-09-17 02:15:17 -0300

    Starting React

---
+++ Front-End/Bloco 10/10.1/hello-world/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Header from './Header.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Header />
+);
+
